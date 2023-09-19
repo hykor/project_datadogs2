3b3a70716e47517cfbbc705c1634eed74393a843
hykor <wisit_48140@hotmail.com>
2023-09-19 10:56:52 +0700

    update first day

--- fontend/application/config/config.php
+++ fontend/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].'/project_datadogsBeta/fontend';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/project_datadogs2/fontend';
 
 /*
 |--------------------------------------------------------------------------
