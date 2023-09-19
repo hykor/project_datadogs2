e97d3222a5f707807ddf5b0824fc2a12e41a1919
hykor <wisit_48140@hotmail.com>
2023-09-19 10:58:18 +0700

    update

--- fontend/application/config/config.php
+++ fontend/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/project_datadogs2/fontend';
+// $config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/project_datadogs2/fontend';
+$config['base_url'] = 'http://localhost/project_datadogs2/fontend';
 
 /*
 |--------------------------------------------------------------------------
