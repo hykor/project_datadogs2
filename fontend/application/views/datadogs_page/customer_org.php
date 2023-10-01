7f2f19893dd5df326da8112a7bed456a4b9024b1
hykor <wisit_48140@hotmail.com>
2023-10-01 19:30:03 +0700

    update

--- fontend/application/views/datadogs_page/customer_org.php
+++ fontend/application/views/datadogs_page/customer_org.php
@@ -1,120 +1,166 @@
 <div class="row page-titles">
-	<div class="col-md-5 align-self-center">
-		<h4 class="text-themecolor">Organization</h4>
-	</div>
+    <div class="col-md-5 align-self-center">
+        <h4 class="text-themecolor">Organization</h4>
+    </div>
 
-	<div class="">
-		<button class="right-side-toggle waves-effect waves-light btn-inverse btn btn-circle btn-sm pull-right m-l-10"><i class="ti-settings text-white"></i></button>
-	</div>
+    <div class="">
+        <button class="right-side-toggle  btn waves-effect waves-light btn-outline-secondary m-l-10 "><i class="ti-filter"></i><span>Filter</span></button>
+    </div>
 </div>
 
 <div class="row">
-	<div class="col-md-12">
-		<div class="card">
-			<div class="card-body">
-				<div class="row d-flex">
-					<div class="col-md-12">
-						<h4 class="card-title"><span class="lstick"></span>Customer Organization</h4>
-					</div>
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row d-flex">
+                    <div class="col-md-12">
+                        <h4 class="card-title"><span class="lstick"></span>Customer Organization</h4>
+                    </div>
 
-					<div class="col-md-12 mt-1">
-						<h4 class="card-title">ANW <a class="get-code" data-toggle="collapse" href="#tt1" aria-expanded="true"><i class="fa fa-sort-desc has-arrow waves-effect waves-dark" title="List Customer org" data-toggle="tooltip"></i></a></h4>
-                        <div class="collapse m-t-15" id="tt1" aria-expanded="true"> 
-							<div class="table-responsive">
-                                <table class="table table-hover">
+                    <div class="col-md-12 mt-1">
+                        <h6 class="card-title">AWN <a class="get-code" data-toggle="collapse" href="#tt1" aria-expanded="true"><i class="fa fa-sort-desc has-arrow waves-effect waves-dark" title="List Customer org" data-toggle="tooltip"></i></a></h6>
+                        <div class="collapse show m-t-15" id="tt1" aria-expanded="true">
+                            <div class="table-responsive">
+                                <table class="table table-hover table-bordered">
                                     <thead>
                                         <tr>
-                                            <th>#</th>
-                                            <th>Customer Org</th>
-                                            <th>DATA1</th>
-                                            <th>DATA2</th>
+                                            <th>Date</th>
+                                            <th>Subscription</th>
+                                            <th>Unit</th>
+                                            <th>Usage</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td>1</td>
-                                            <td>User1</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,-3,-2,-4,-5,-4,-3,-2,-5,-1</span> </td>
-                                            <td><span class="text-danger text-semibold"><i class="fa fa-level-down" aria-hidden="true"></i> 28.76%</span> </td>
+                                            <td>2023-04</td>
+                                            <td>Infra Host</td>
+                                            <td>Per Host</td>
+                                            <td>80</td>
                                         </tr>
                                         <tr>
-                                            <td>2</td>
-                                            <td>User2</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,-1,-1,-2,-3,-1,-2,-3,-1,-2</span> </td>
-                                            <td><span class="text-warning text-semibold"><i class="fa fa-level-down" aria-hidden="true"></i> 8.55%</span> </td>
+                                            <td>2023-04</td>
+                                            <td>Container</td>
+                                            <td>Per Container</td>
+                                            <td>743</td>
                                         </tr>
                                         <tr>
-                                            <td>3</td>
-                                            <td>User3</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,3,6,1,2,4,6,3,2,1</span> </td>
-                                            <td><span class="text-success text-semibold"><i class="fa fa-level-up" aria-hidden="true"></i> 58.56%</span> </td>
+                                            <td>2023-04</td>
+                                            <td>APM</td>
+                                            <td>Per Container</td>
+                                            <td>42</td>
                                         </tr>
                                         <tr>
-                                            <td>4</td>
-                                            <td>User4</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,3,6,4,5,4,7,3,4,2</span> </td>
-                                            <td><span class="text-info text-semibold"><i class="fa fa-level-up" aria-hidden="true"></i> 35.76%</span> </td>
+                                            <td>2023-04</td>
+                                            <td>Database</td>
+                                            <td>Per Host</td>
+                                            <td>40</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2023-04</td>
+                                            <td>Log Indexing 30D</td>
+                                            <td>Per 1M. Event</td>
+                                            <td>530</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2023-04</td>
+                                            <td>Log Indexing 7D</td>
+                                            <td>Per 1M. Event</td>
+                                            <td>0</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2023-04</td>
+                                            <td>Log Indexing 3 D</td>
+                                            <td>Per 1M. Event</td>
+                                            <td>3280</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2023-04</td>
+                                            <td>Log Ingestion</td>
+                                            <td>Per 1 GB</td>
+                                            <td>25900</td>
                                         </tr>
                                     </tbody>
                                 </table>
                             </div>
-						</div>
-					</div>
-					
-					<div class="col-md-12 mt-1">
-						<h4 class="card-title">CPN <a class="get-code" data-toggle="collapse" href="#tt2" aria-expanded="true"><i class="fa fa-sort-desc" title="List Customer org" data-toggle="tooltip"></i></a></h4>
-                        <div class="collapse m-t-15" id="tt2" aria-expanded="true"> 
-							<div class="table-responsive">
-                                <table class="table table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Customer Org</th>
-                                            <th>DATA1</th>
-                                            <th>DATA2</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>User1</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,-3,-2,-4,-5,-4,-3,-2,-5,-1</span> </td>
-                                            <td><span class="text-danger text-semibold"><i class="fa fa-level-down" aria-hidden="true"></i> 28.76%</span> </td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>User2</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,-1,-1,-2,-3,-1,-2,-3,-1,-2</span> </td>
-                                            <td><span class="text-warning text-semibold"><i class="fa fa-level-down" aria-hidden="true"></i> 8.55%</span> </td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>User3</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,3,6,1,2,4,6,3,2,1</span> </td>
-                                            <td><span class="text-success text-semibold"><i class="fa fa-level-up" aria-hidden="true"></i> 58.56%</span> </td>
-                                        </tr>
-                                        <tr>
-                                            <td>4</td>
-                                            <td>User4</td>
-                                            <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40">0,3,6,4,5,4,7,3,4,2</span> </td>
-                                            <td><span class="text-info text-semibold"><i class="fa fa-level-up" aria-hidden="true"></i> 35.76%</span> </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-						</div>
-					</div>
+                        </div>
+                    </div>
 
-				</div>
-			</div>
-		</div>
+                </div>
+            </div>
+        </div>
 
-	</div>
+    </div>
 </div>
 
 
+<!-- .right-sidebar -->
+<div class="right-sidebar">
+    <div class="slimscrollright">
+        <div class="rpanel-title"> Filter Organization <span><i class="ti-close right-side-toggle"></i></span> </div>
+        <div class="r-panel-body">
+            <ul class="m-t-20">
+                <li><b>Custommer</b></li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_1" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_1">AWN</label>
+                    </b>
+                </li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_2" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_2">DTGO</label>
+                    </b>
+                </li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_3" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_3">Greenline</label>
+                    </b>
+                </li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_4" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_4">Heng</label>
+                    </b>
+                </li>
+            </ul>
+
+            <ul class="m-t-20 chatonline">
+                <li><b>Usage</b></li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_5" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_5">Infra Host</label>
+                    </b>
+                </li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_6" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_6">infra_container</label>
+                    </b>
+                </li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_7" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_7">apm_host</label>
+                    </b>
+                </li>
+                <li>
+                    <b class="">
+                        <input type="checkbox" id="basic_checkbox_8" class="filled-in chk-col-indigo" checked />
+                        <label for="basic_checkbox_8">logs_ingested</label>
+                    </b>
+                </li>
+            </ul>
+        </div>
+
+
+
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Right sidebar -->
+
+
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-
-
-
-
